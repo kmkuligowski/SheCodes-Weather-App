2a70734ca71e8ee277cee9fdc488d44d3aa1a6f2
kmkuligowski <74575914+kmkuligowski@users.noreply.github.com>
2021-01-09 17:24:35 -0500

    hourly forecast added
    with some changes to CSS styles

--- src/javascript.js
+++ src/javascript.js
@@ -13,6 +13,16 @@ if (mins<10){
  return `${day}, ${hour}:${mins}`;
 }
 
+function formatTime(timestamp){
+let date= new Date(timestamp);
+let hours=date.getHours();
+if(hours<10){
+  hours=`0${hours}`;
+}
+let minutes=date.getMinutes();
+if (minutes<10){minutes=`0${minutes}`}
+return `${hours}:${minutes}`;}
+
 function showTemp(response){
    let iconElement= document.querySelector("#icon");
 
@@ -30,10 +40,29 @@ function showTemp(response){
  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`) 
 }
 
+function displayForecast(response){
+  let forecastElement= document.querySelector("#forecast");
+  forecastElement.innerHTML=null;
+  let forecast=null;
+
+  for (let index = 0; index < 6; index++){
+    let forecast= response.data.list[index];
+    forecastElement.innerHTML+=`
+    <div class="col-2">
+    <h4>${formatTime(forecast.dt*1000)}</h4>
+    <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" alt="weather image">   
+    <h4>${Math.round(forecast.main.temp_max)}°</h4>
+    </div>`}}
+
+
+
 function searchCity(city){
-let apiKey ="0ee65006a00c913e43ac0bf2c3ea0b12";
+  let apiKey ="0ee65006a00c913e43ac0bf2c3ea0b12";
   let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
   axios.get(`${apiUrl}`).then(showTemp);
+
+  apiUrl=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+  axios.get(`${apiUrl}`).then(displayForecast);
 }
 
 function submitCity(event){
